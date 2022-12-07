e36b5b8a153dd872f69d783878d739c56b3865b5
Aryan <99786026+Mortal22Soul@users.noreply.github.com>
2022-12-07 21:05:50 +0530

    Added files

--- script.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.getElementsByClassName("hamburger")[0];
-const navbarLinks = document.getElementsByClassName("expanded-hamburger")[0];
-
-toggleButton.addEventListener("click", () => {
-  navbarLinks.classList.toggle("active");
-});
